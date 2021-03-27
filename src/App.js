06834d98e6e6bf9759ae6409656452e5e54550ae
Daniel Mizerski <duelsik@gmail.com>
2021-03-27 22:58:19 +0100

    Throw away readme, highlight rains

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 const url = "https://api.openweathermap.org/data/2.5/onecall?lat=53.4289&lon=14.553&exclude=minutely,daily,alerts&units=metric&appid=035c2a38881a80dff29561d6b59a2bda";
@@ -9,16 +8,17 @@ const transformData = responseData => {
     return {
       formattedDate: (new Date(data.dt * 1000)).toLocaleString("pl-PL"),
       temp: data.temp,
+      tempFeel: data.feels_like,
       weather: data.weather[0].main
     };
   });
 }
 
-const WeatherCell = ({ formattedDate, temp, weather }) => (
+const WeatherCell = ({ formattedDate, temp, weather, tempFeel }) => (
   <div className={formattedDate.includes("06:00:00") ? "marszowagodzina" : ""}>
     <div>{ formattedDate }</div>
-    <div>{ temp }</div>
-    <div>{ weather }</div>
+    <div>{ temp } ({tempFeel})</div>
+    <div className={weather === "Rain" ? "rain" : ""}>{ weather }</div>
   </div>
 );
 
