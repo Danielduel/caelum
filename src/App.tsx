8149c011f256931b70f086ef7ed06109a1a3af5c
Daniel Mizerski <duelsik@gmail.com>
2021-03-30 14:50:39 +0200

    Add moment and mnemonic calendar date

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import cn from "classnames";
+import moment from "moment";
+import "moment/locale/pl";
 import * as OpenWeatherMapForecastAPI from "./typings/OpenWeatherMapForecastAPI";
 import "./App.css";
 type Unpacked<T> = T extends (infer U)[] ? U : T;
+moment.locale("pl");
 
 const url =
   "https://api.openweathermap.org/data/2.5/onecall?lat=53.4289&lon=14.553&exclude=minutely,daily,alerts&units=metric&appid=035c2a38881a80dff29561d6b59a2bda";
 const iconUrl = (code: string) => `http://openweathermap.org/img/wn/${code}.png`;
 
+const transformFormattedDateMnemonic = (date: Date) => {
+  return moment(date).calendar();
+};
+
 const transformData = (responseData: OpenWeatherMapForecastAPI.ResponseData) => {
   return responseData.hourly.map((data) => {
     return {
+      formattedDateMnemonic: transformFormattedDateMnemonic(new Date(data.dt * 1000)),
       formattedDate: new Date(data.dt * 1000).toLocaleString("pl-PL"),
       temp: data.temp,
       tempFeel: data.feels_like,
@@ -22,7 +30,7 @@ const transformData = (responseData: OpenWeatherMapForecastAPI.ResponseData) => 
 type TransformData = ReturnType<typeof transformData>;
 type TransformDataItem = Unpacked<TransformData>;
 
-const WeatherRow = ({ formattedDate, temp, weather, tempFeel, icon }: TransformDataItem) => (
+const WeatherRow = ({ formattedDate, formattedDateMnemonic, temp, weather, tempFeel, icon }: TransformDataItem) => (
   <div
     className={cn("weather-row", {
       marszowagodzina: formattedDate.includes("06:00:00")
@@ -30,6 +38,7 @@ const WeatherRow = ({ formattedDate, temp, weather, tempFeel, icon }: TransformD
     key={formattedDate}
   >
     <div className="weather-row__formattedDate">{formattedDate}</div>
+    <div className={cn("weather-row__formattedDateMnemonic", "mobile-only")}>{formattedDateMnemonic}</div>
     <div className="weather-row__temp">
       <span className="weather-row__temp-temp">temp&nbsp;</span>
       {temp}
