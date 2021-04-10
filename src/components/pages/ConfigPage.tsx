f5ebdcb7aa0a4d7e488e0eabfd60624c34b85310
Daniel Podolak <cutler@ncdc.pl>
2021-04-11 00:28:45 +0200

    added dragula support

--- src/components/pages/ConfigPage.tsx
+++ src/components/pages/ConfigPage.tsx
@@ -1,5 +1,29 @@
+import React from "react";
+import "./ConfigPage.css";
+import dragula from "dragula";
+
+const DrawerItem: React.FunctionComponent = () => {
+  return <div className="drawer-item">item</div>;
+};
+
 const ConfigPage: React.FunctionComponent = () => {
-  return <div>Config</div>;
+  const listContainer = React.useRef<HTMLDivElement>(null);
+  const gridContainer = React.useRef<HTMLDivElement>(null);
+  React.useEffect(() => {
+    if (listContainer.current && gridContainer.current) {
+      dragula([listContainer.current, gridContainer.current]);
+    }
+  }, [listContainer, gridContainer]);
+  return (
+    <div className="config-container">
+      <div ref={listContainer} className="widget-list-container">
+        <DrawerItem />
+        <DrawerItem />
+        <DrawerItem />
+      </div>
+      <div ref={gridContainer} className="grid-container"></div>
+    </div>
+  );
 };
 
 export default ConfigPage;
