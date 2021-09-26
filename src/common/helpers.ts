1dd44eb0ee6954d109f5a53d86eb425cadcdb04c
Daniel Mizerski <duelsik@gmail.com>
2021-09-26 14:44:00 +0200

    (feature) S11lL21I Create basic layout.

---
+++ src/common/helpers.ts
@@ -0,0 +1,13 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+/*
+  Most likely this file will be split in future, just store handy things here
+  treat it as constants/common/utils
+  if something is more than 3 lines - export to separate file
+*/
+
+export const emptyCss = css``;
+export const emptyDiv = styled.div``;
+export type StyledComponentsCss = ReturnType<typeof css>;
+export type WithChildren = { children: React.ReactNode };
