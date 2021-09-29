eb45e61b45f6ee6ea3b76efb3a108ca255e3ab68
Daniel Mizerski <duelsik@gmail.com>
2021-09-30 00:39:56 +0200

    (feature) Add landscape to front page.

--- src/components/TodayWeather.tsx
+++ src/components/TodayWeather.tsx
@@ -2,16 +2,42 @@ import React from "react";
 import { CurrentWeatherForecast, HourWeatherForecast } from "../models/OpenWeatherAPI";
 import styled from "styled-components";
 import { FormattedHour, FormattedTemperature } from "./ValueFormatters";
+import prototypeLandscapeSvg from "./landscapes/prototype.svg";
 
-const TodayWeatherContainer = styled.div`
+const TodayWeatherWrapper = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
-  padding: 2rem;
   box-sizing: border-box;
 
   & > * {
     flex: 1;
+  }
+`;
+const TodayWeatherBackgroundContainer = styled.div`
+  position: relative;
+`;
+const TodayWeatherBackground = styled.img`
+  width: 100%;
+`;
+const TodayWeatherBackgroundInfo = styled.div`
+  position: absolute;
+  top: 3rem;
+  left: 3rem;
+  color: white;
+  text-shadow: -.2px -.2px 0 #000, .2px -.2px 0 #000, -.2px .2px 0 #000, .2px .2px 0 #000;
+  font-weight: 500;
+  
+  transform: scale(5);
+  transform-origin: 0 0;
+`;
+const TodayWeatherBackgroundInfoIcon = styled.i``;
+const TodayWeatherBackgroundInfoLine = styled.div`
+  font-size: 0.8em;
+  font-weight: 900;
+  /* Bad solution (I don't know why formatted date is a span) */
+  & > span > span {
+    font-size: 0.6em;
   }
 `;
 
@@ -23,13 +49,16 @@ type TodayWeatherProps = {
 export const TodayWeather: React.FunctionComponent<TodayWeatherProps> = (props) => {
   const range = Array(6).fill(1);
   return (
-    <TodayWeatherContainer>
-      <div>
-        <h1>
-          <FormattedTemperature value={props.currentWeather.temp} />
-          <i className="wi wi-night-sleet" />
-        </h1>
-      </div>
+    <TodayWeatherWrapper>
+      <TodayWeatherBackgroundContainer>
+        <TodayWeatherBackground src={prototypeLandscapeSvg} />
+        <TodayWeatherBackgroundInfo>
+          <TodayWeatherBackgroundInfoIcon className="wi wi-night-sleet" />
+          <TodayWeatherBackgroundInfoLine>
+            <FormattedTemperature value={props.currentWeather.temp} />
+          </TodayWeatherBackgroundInfoLine>
+        </TodayWeatherBackgroundInfo>
+      </TodayWeatherBackgroundContainer>
       <div>
         {range.map((_, idx) => {
           return (
@@ -41,6 +70,6 @@ export const TodayWeather: React.FunctionComponent<TodayWeatherProps> = (props) 
           );
         })}
       </div>
-    </TodayWeatherContainer>
+    </TodayWeatherWrapper>
   );
 };
