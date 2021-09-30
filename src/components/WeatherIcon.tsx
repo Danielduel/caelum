2ab4afacd19b3dd346c0fb6642d02d86ab700056
Daniel Mizerski <duelsik@gmail.com>
2021-09-30 20:54:32 +0200

    (feature) Hourly forecast.

--- src/components/WeatherIcon.tsx
+++ src/components/WeatherIcon.tsx
@@ -1,11 +1,25 @@
-import { weatherIconsConfig } from "../App";
 import React from "react";
+import cn from "classnames";
+import { weatherIconsConfig } from "../App";
+import { StyledClassName } from "../common/helpers";
+import { WeatherInfo } from "../models/OpenWeatherAPI";
 
 const getDayIcon = (weatherCode: number) => weatherIconsConfig[weatherCode][3];
 const getNightIcon = (weatherCode: number) => weatherIconsConfig[weatherCode][4];
 
-export const WeatherIcon = (props: { weatherCode: number }): JSX.Element => {
+type WeatherIconProps = StyledClassName & {
+  weatherCode: number;
+};
+export const WeatherIcon = ({ weatherCode, className }: WeatherIconProps): JSX.Element => {
   const now = new Date().getHours();
-  const icon = now > 5 && now < 22 ? getDayIcon(props.weatherCode) : getNightIcon(props.weatherCode);
-  return <i className={`wi ${icon}`} />;
+  const icon = now > 5 && now < 22 ? getDayIcon(weatherCode) : getNightIcon(weatherCode);
+  return <i className={cn("wi", icon, className)} />;
+};
+
+type WeatherInfoArrWeatherIconProps = StyledClassName & {
+  weatherInfoArr: WeatherInfo[];
+};
+export const WeatherInfoArrWeatherIcon = ({ weatherInfoArr, className }: WeatherInfoArrWeatherIconProps) => {
+  const [weatherInfo] = weatherInfoArr;
+  return <WeatherIcon weatherCode={weatherInfo.id} className={className} />;
 };
