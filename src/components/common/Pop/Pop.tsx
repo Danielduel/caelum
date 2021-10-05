dcc84611d833a82e69838c1ec7efc9383456bff1
Daniel Mizerski <duelsik@gmail.com>
2021-10-05 23:31:43 +0200

    (feature) Fill pop icon.

--- src/components/common/Pop/Pop.tsx
+++ src/components/common/Pop/Pop.tsx
@@ -1,12 +1,52 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { StyledClassName } from "../../../common/helpers";
 import dropletIcon from "./droplet-icon.svg";
 
 const PopWrapper = styled.div`
   position: relative;
 `;
+const PopFill = styled.div`
+  line-height: 0;
+  display: inline-block; /* Nongreedy space */
+  mix-blend-mode: multiply;
+  background: transparent;
+`;
+type PopIconWrapperProps = { popValue: number };
+const linearGradientWithPopValue = ({ popValue }: PopIconWrapperProps) => {
+  const wetColor = "cyan";
+  const dryColor = "#eee";
+  if (popValue <= 0) {
+    return css`
+      background: ${dryColor};
+    `;
+  }
+  if (popValue >= 100) {
+    return css`
+      background: ${wetColor};
+    `;
+  }
+  const gradientOffset = 5;
+  const gradientDistance = 10;
+  const minStop = Math.ceil(Math.max(Math.min(popValue - gradientOffset - gradientDistance / 2, 100), 0));
+  const maxStop = Math.ceil(Math.max(Math.min(popValue - gradientOffset + gradientDistance / 2, 100), 0));
+  return css`
+    background: linear-gradient(
+      0deg,
+      ${wetColor} 0%,
+      ${wetColor} ${minStop}%,
+      ${dryColor} ${maxStop}%,
+      ${dryColor} 100%
+    );
+  `;
+};
+const PopIconWrapper = styled.div`
+  mix-blend-mode: screen;
+  isolation: isolate;
+  ${linearGradientWithPopValue}
+`;
 const PopIcon = styled.img`
-  opacity: 0.1;
+  mix-blend-mode: screen;
+  background: white;
   width: 4em;
 `;
 const PopText = styled.div`
@@ -20,10 +60,15 @@ type PopProps = StyledClassName & {
   pop: number;
 };
 const Pop = ({ pop, className }: PopProps) => {
+  const popValue = Math.ceil(pop * 100);
   return (
     <PopWrapper className={className}>
-      <PopIcon src={dropletIcon} />
-      <PopText>{Math.ceil(pop * 100)}%</PopText>
+      <PopFill>
+        <PopIconWrapper popValue={popValue}>
+          <PopIcon src={dropletIcon} />
+        </PopIconWrapper>
+      </PopFill>
+      <PopText>{popValue}%</PopText>
     </PopWrapper>
   );
 };
