7184b70320066cc6bc5794a5d270b39759653c75
Daniel Mizerski <duelsik@gmail.com>
2021-10-15 23:03:56 +0200

    (feature) Add BasicInfo component.

---
+++ src/components/BasicInfo/BasicInfo.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { WeatherIcon } from "../WeatherIcon";
+import { FormattedTemperature } from "../ValueFormatters";
+import { weatherZeroDescription, weatherZeroId } from "../../common/helpers";
+import { CurrentWeatherForecast, HourWeatherForecast, WeatherInfo } from "../../models/OpenWeatherAPI";
+
+const BasicInfoWrapper = styled.div`
+  font-family: "Roboto", sans-serif;
+`;
+const BasicInfoHeadline = styled.div``;
+const Separator = styled.span`
+  /* Don't ask plz */
+  margin-left: 1em;
+`;
+const BasicInfoIcon = styled(WeatherIcon)`
+  font-size: 6em;
+  /* font-weight: 100; - figure out a way to reduce THICCCCCNESS of those icons */
+`;
+const BasicInfoTemperature = styled(FormattedTemperature)`
+  font-size: 3em;
+  font-weight: 1000; /* doesn't follow design, should be 500 */
+`;
+const BasicInfoDescription = styled.div`
+  font-size: 2.25em;
+  font-weight: 1000; /* doesn't follow design, should be 500 */
+
+  &:first-letter {
+    text-transform: capitalize;
+  }
+`;
+type BasicInfoProps = {
+  temp: HourWeatherForecast["temp"];
+  description: WeatherInfo["description"];
+  weatherCode: WeatherInfo["id"];
+};
+export const BasicInfo = ({ temp, description, weatherCode }: BasicInfoProps) => {
+  return (
+    <BasicInfoWrapper>
+      <BasicInfoHeadline>
+        <BasicInfoIcon weatherCode={weatherCode} />
+        <Separator />
+        <BasicInfoTemperature value={temp} withoutSpace />
+      </BasicInfoHeadline>
+      <BasicInfoDescription>{description}</BasicInfoDescription>
+    </BasicInfoWrapper>
+  );
+};
+
+type BasicInfoFromCurrentWeatherForecastProps = { currentWeather: Pick<CurrentWeatherForecast, "temp" | "weather"> };
+export const BasicInfoFromCurrentWeatherForecast = ({
+  currentWeather: { temp, weather }
+}: BasicInfoFromCurrentWeatherForecastProps) => (
+  <BasicInfo temp={temp} description={weatherZeroDescription(weather)} weatherCode={weatherZeroId(weather)} />
+);
--- src/pages/TodayWeather.tsx
+++ src/pages/TodayWeather.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import styled from "styled-components";
-import { FormattedTemperature } from "../components/ValueFormatters";
 import prototypeLandscapeSvg from "../components/landscapes/prototype.svg";
-import { WeatherIcon } from "../components/WeatherIcon";
 import { HourlyForecast } from "../components/HourlyForecast/HourlyForecast";
 import { CurrentWeatherForecast, HourWeatherForecast } from "../models/OpenWeatherAPI";
+import { BasicInfoFromCurrentWeatherForecast } from "../components/BasicInfo/BasicInfo";
 
 export type TodayWeatherProps = {
   currentWeather: CurrentWeatherForecast;
@@ -12,50 +10,24 @@ export type TodayWeatherProps = {
 };
 
 const TodayWeatherWrapper = styled.div`
+  height: 100%;
+  padding-top: 2em;
+  padding-left: 1.5em;
+  box-sizing: border-box;
+
+  background-image: url(${prototypeLandscapeSvg});
+  background-size: contain;
+  background-repeat: no-repeat;
+
   display: flex;
   flex-direction: column;
-  height: 100%;
-  box-sizing: border-box;
-`;
-const TodayWeatherBackgroundContainer = styled.div`
-  position: relative;
-`;
-const TodayWeatherBackground = styled.img`
-  width: 100%;
-`;
-const TodayWeatherBackgroundInfo = styled.div`
-  position: absolute;
-  top: 3rem;
-  left: 3rem;
-  color: white;
-  text-shadow: -0.2px -0.2px 0 #000, 0.2px -0.2px 0 #000, -0.2px 0.2px 0 #000, 0.2px 0.2px 0 #000;
-  font-weight: 500;
-
-  transform: scale(5);
-  transform-origin: 0 0;
-`;
-const TodayWeatherBackgroundInfoLine = styled.div`
-  font-size: 0.8em;
-  font-weight: 900;
-  /* Bad solution (I don't know why formatted date is a span) */
-  & > span > span {
-    font-size: 0.6em;
-  }
 `;
 
-export const TodayWeather: React.FunctionComponent<TodayWeatherProps> = (props) => {
+export const TodayWeather = ({ currentWeather, hourForecast }: TodayWeatherProps) => {
   return (
     <TodayWeatherWrapper>
-      <TodayWeatherBackgroundContainer>
-        <TodayWeatherBackground src={prototypeLandscapeSvg} />
-        <TodayWeatherBackgroundInfo>
-          <WeatherIcon weatherCode={props.currentWeather.weather[0].id} />
-          <TodayWeatherBackgroundInfoLine>
-            <FormattedTemperature value={props.currentWeather.temp} />
-          </TodayWeatherBackgroundInfoLine>
-        </TodayWeatherBackgroundInfo>
-      </TodayWeatherBackgroundContainer>
-      <HourlyForecast hourForecast={props.hourForecast} />
+      <BasicInfoFromCurrentWeatherForecast currentWeather={currentWeather} />
+      <HourlyForecast hourForecast={hourForecast} />
     </TodayWeatherWrapper>
   );
 };
