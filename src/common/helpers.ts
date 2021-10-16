bbf0bc5683c28bc7190d08527246bd3ff68781d2
Daniel Mizerski <duelsik@gmail.com>
2021-10-17 00:13:39 +0200

    (feature) Add BasicInfo component. (#35)

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { WeatherInfo } from "../models/OpenWeatherAPI";
 
 /*
   Most likely this file will be split in future, just store handy things here
@@ -15,3 +16,6 @@ export type WithChildren = { children: React.ReactNode };
 export type StyledClassName = { className?: string };
 export const head = (num: number) => (_: unknown, index: number) => index < num;
 export const skipFirst = () => (_: unknown, index: number) => index !== 0;
+export const arrZero = <T>(arr: [T]) => arr[0];
+export const weatherZeroId = (weather: [WeatherInfo]) => arrZero(weather).id;
+export const weatherZeroDescription = (weather: [WeatherInfo]) => arrZero(weather).description;
