b4d62c3b55d164fcef5e2f23923b0d921c948737
Daniel Mizerski <duelsik@gmail.com>
2021-11-01 22:16:47 +0100

    Wrap app in container and enable HMR. (#40)

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+// This enables HMR
+// TODO: check if it affects prod build
+// sorry TS
+if (((module as unknown) as Record<string, unknown>)?.hot) {
+  ((module as unknown) as Record<"hot", { accept: () => void }>)?.hot?.accept();
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
