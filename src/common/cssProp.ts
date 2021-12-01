583b5fbb62930bfb765f4b7309635b919a4d40dc
Daniel Mizerski <duelsik@gmail.com>
2021-12-01 23:03:01 +0100

    Remove clg

--- src/common/cssProp.ts
+++ src/common/cssProp.ts
@@ -6,7 +6,6 @@ import { prop } from "./helpers";
 export const cssProp =
   <T, K extends keyof T>(cssName: keyof Properties, attrName: K, defaultValue = "") =>
   (props: T) => {
-    console.log(cssName, attrName, defaultValue, prop<T, K>(attrName, defaultValue)(props));
     // not sure why more direct form of that is bugging intellisense
     const _css = `${cssName}: ${prop<T, K>(attrName, defaultValue)(props)};`;
     return css`
