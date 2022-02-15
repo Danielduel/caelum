f7d858101fbaa9570ad2b669328a11f566b0fc8e
Daniel Podolak <podolak9602@gmail.com>
2022-02-15 23:56:54 +0100

    code review

--- src/components/RainWindInfo/WindDescription.tsx
+++ src/components/RainWindInfo/WindDescription.tsx
@@ -10,19 +10,19 @@ export type WindDescriptionProps = StyledClassName & {
 export type WindSpeedDescription = {
   min: number;
   max: number;
-  label: string;
+  i18nLabelKey: string;
 };
 
 const WindSpeedValues: WindSpeedDescription[] = [
-  { min: 0, max: 0.499, label: "windDescr_1" },
-  { min: 0.5, max: 5.399, label: "windDescr_2" },
-  { min: 5.4, max: 7.999, label: "windDescr_3" },
-  { min: 8, max: 13.799, label: "windDescr_4" },
-  { min: 13.8, max: 16.999, label: "windDescr_5" },
-  { min: 17, max: 20.799, label: "windDescr_6" },
-  { min: 20.8, max: 24.499, label: "windDescr_7" },
-  { min: 24.5, max: 29.999, label: "windDescr_8" },
-  { min: 30, max: 1000, label: "windDescr_9" }
+  { min: 0, max: 0.499, i18nLabelKey: "windDescr_1" },
+  { min: 0.5, max: 5.399, i18nLabelKey: "windDescr_2" },
+  { min: 5.4, max: 7.999, i18nLabelKey: "windDescr_3" },
+  { min: 8, max: 13.799, i18nLabelKey: "windDescr_4" },
+  { min: 13.8, max: 16.999, i18nLabelKey: "windDescr_5" },
+  { min: 17, max: 20.799, i18nLabelKey: "windDescr_6" },
+  { min: 20.8, max: 24.499, i18nLabelKey: "windDescr_7" },
+  { min: 24.5, max: 29.999, i18nLabelKey: "windDescr_8" },
+  { min: 30, max: 1000, i18nLabelKey: "windDescr_9" }
 ];
 
 export const WindDescriptionWrapper = styled.span``;
@@ -33,7 +33,7 @@ export const WindDescription = ({ windSpeed, className }: WindDescriptionProps):
     [windSpeed]
   );
   const defaultWindSpeedValue = arrZero(WindSpeedValues);
-  const { label } = item || defaultWindSpeedValue;
+  const { i18nLabelKey } = item || defaultWindSpeedValue;
   const { t } = useTranslation();
-  return <WindDescriptionWrapper className={className}>({t(label)})</WindDescriptionWrapper>;
+  return <WindDescriptionWrapper className={className}>({t(i18nLabelKey)})</WindDescriptionWrapper>;
 };
