7f2341154047fdc4dd20c6980e97dfc047f87137
Daniel Podolak <podolak9602@gmail.com>
2022-05-14 11:02:51 +0200

    updated integration tests

--- src/components/layouts/AppContainer.tsx
+++ src/components/layouts/AppContainer.tsx
@@ -3,7 +3,7 @@ import backgroundForest from "../../assets/img/background_forest.svg";
 
 export const AppContainer = styled.div`
   scroll-snap-type: y mandatory;
-  //overflow: scroll;
+  overflow: scroll;
   height: 100vh;
   background-image: url(${backgroundForest});
   background-size: cover;
